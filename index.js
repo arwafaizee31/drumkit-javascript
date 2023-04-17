c929465663d50e280c767d603ff5d9e3837668c5
arwa <arwafaizee142@gmail.com>
2023-04-17 18:33:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+
+for(var i = 0 ; i<document.getElementsByClassName("drum").length;i++){
+    document.getElementsByClassName("drum")[i].addEventListener("click" , function(){
+var buttonih = this.innerHTML;
+makeSound(buttonih);
+makePress(buttonih);
+
+
+    })
+}
+
+
+document.addEventListener("keydown" , function(event){
+    makeSound(event.key);
+    makePress(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+   
+
+        case "w":
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    tom1.play();
+    break;
+
+    case "a":
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    tom2.play();
+    break;
+
+    case "s":
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    tom3.play();
+    break;
+
+    case "d":
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    tom4.play();
+break;
+
+    case "j":
+    var snare = new Audio('sounds/snare.mp3');
+    snare.play();
+    break;
+
+    case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+break;
+
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+
+            kick.play();
+            break;
+
+            default:
+                alert("wrong");
+}
+
+
+}
+
+function makePress(currentkey){
+    document.querySelector("." + currentkey).classList.add("pressed");
+    setTimeout(  function(){
+        document.querySelector("." + currentkey).classList.remove("pressed");
+    } , 100);
+}
+
+            // default:
+            //     alert("wrong");
+
+// document.addEventListener("keydown" , function(key){
+//     console.log(key);
+// })
+
+// }
+// document.addEventListener("click" , function(){
+//     this.style.color = "blue";
+// })
+
+    
+
+//we dont use paranthesis there as the func will run directly
+// var audio1 = new Audio('sounds/tom-1.mp3');
+//     audio1.play();
